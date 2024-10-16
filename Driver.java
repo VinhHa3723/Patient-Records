891e617de9b06ef737a85a8b0c05fdb3372d94bf
VinhHa3723 <vinh.ha3723@gmail.com>
2024-10-15 23:33:03 -0500

    Driver.java

---
+++ Driver.java
@@ -0,0 +1,66 @@
+/** 
+   A driver that demonstrates the class PatientRecord.
+ */
+public class Driver
+{
+	public static void main(String args[])
+	{
+		PatientRecord test1;
+		PatientRecord test2;
+
+		try
+		{
+			test1 = new PatientRecord(101, 12, 15, 2020, "cough", "bed rest");
+			System.out.println("Patient Record created:  " + test1);
+			System.out.println("It has hash code " + test1.hashCode());
+		} // end try
+		catch(BadVisitDateException e)
+		{
+			System.out.println("Creation failed " + e);
+		} // end catch
+
+		try
+		{
+			test2 = new PatientRecord(101, 12, 17, 2020, "high fever", "antibiotics");
+			System.out.println("Patient Record created:  " + test2);
+			System.out.println("It has hash code " + test2.hashCode());
+		} // end try
+		catch(BadVisitDateException e)
+		{
+			System.out.println("Creation failed " + e);
+		} // end catch
+
+		try
+		{
+			test2 = new PatientRecord(101, 17, 17, 2020, "high fever", "antibiotics");
+			System.out.println("Patient Record created:  " + test2);
+			System.out.println("It has hash code " + test2.hashCode());
+		} // end try
+		catch(BadVisitDateException e)
+		{
+			System.out.println("Creation failed " + e);
+		} // end catch
+
+		try
+		{
+			test2 = new PatientRecord(101, 12, 92, 2020, "high fever", "antibiotics");
+			System.out.println("Patient Record created:  " + test2);
+			System.out.println("It has hash code " + test2.hashCode());
+		} // end try
+		catch(BadVisitDateException e)
+		{
+			System.out.println("Creation failed " + e);
+		} // end catch
+
+		try
+		{
+			test2 = new PatientRecord(101, 12, 17, 20, "high fever", "antibiotics");
+			System.out.println("Patient Record created:  " + test2);
+			System.out.println("It has hash code " + test2.hashCode());
+		} // end try
+		catch(BadVisitDateException e)
+		{
+			System.out.println("Creation failed " + e);
+		} // end catch
+	} // end main
+} // end Driver
